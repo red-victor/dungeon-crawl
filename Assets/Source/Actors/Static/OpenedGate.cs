db778617cea8e64c451178e11826940eb63e6ec8
AdiDD <adi.deaconu21@gmail.com>
2021-09-07 12:03:14 +0300

    Add OpenedGate Actor

---
+++ Assets/Source/Actors/Static/OpenedGate.cs
@@ -0,0 +1,16 @@
+﻿using DungeonCrawl.Core;
+using DungeonCrawl.Actors.Characters;
+
+namespace DungeonCrawl.Actors.Static
+{
+    public class OpenedGate : Actor
+    {
+        public override bool OnCollision(Actor anotherActor)
+        {
+            return true;
+        }
+
+        public override int DefaultSpriteId => 539;
+        public override string DefaultName => "OpenedGate";
+    }
+}
